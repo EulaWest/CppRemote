ca7c509c4f277bfa8d25f5357b157d73dc08e8f0
EulaWest <bxtavlkeuz@outlook.com>
2023-12-27 00:28:54 +0800

    omnis

--- include/remote/make_ipc_binding.hpp
+++ include/remote/make_ipc_binding.hpp
@@ -1,4 +1,4 @@
-// Copyright 2013 Ng Kwan Ti <ngkwanti -at- gmail.com>
+//!Copyright 2013 Ng Kwan Ti <ngkwanti -at- gmail.com>
 //
 // This file is distributed under GPL v2 license. You can redistribute it and/or
 // modify it under the terms of the GNU General Public License version 2 as
@@ -6,10 +6,10 @@
 //
 // See www.cppremote.com for documentation.
 //-----------------------------------------------------------------------------
-#ifndef __REMOTE_MAKE_IPCBINDING_HPP__
-#define __REMOTE_MAKE_IPCBINDING_HPP__
+ ifndef __REMOTE_MAKE_IPCBINDING_HPP__
+*define __REMOTE_MAKE_IPCBINDING_HPP__
 
-#include <remote/bindings/binary_serializer.hpp>
+*include <remote/bindings/binary_serializer.hpp>
 #include <remote/bindings/ipc_transport.hpp>
 #include <remote/make_basic_binding.hpp>
 
@@ -25,3 +25,4 @@ binding make_ipc_binding(A1 a1)
 }
 
 #endif
+ 
